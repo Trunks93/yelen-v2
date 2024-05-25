59aad13e706c3e17c94f5f7c51277247074b5fd7
Saint Cyr <cyr.kpe@smile.ci>
2024-05-25 16:35:11 +0000

    Remove settings.local.php from git and updating settings.php

--- src/web/sites/default/settings.php
+++ src/web/sites/default/settings.php
@@ -885,20 +885,20 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-// $databases['default']['default'] = array (
-//   'database' => 'yelen2',
-//   'username' => 'yelen2',
-//   'password' => ')AQ=4W+Hd7O0QO&8',
-//   'prefix' => '',
-//   'host' => '192.168.2.34',
-//   'port' => '3306',
-//   'isolation_level' => 'READ COMMITTED',
-//   'driver' => 'mysql',
-//   'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-//   'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-// );
-if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-  include $app_root . '/' . $site_path . '/settings.local.php';
- }
+$databases['default']['default'] = array (
+  'database' => 'yelen2',
+  'username' => 'yelen2',
+  'password' => ')AQ=4W+Hd7O0QO&8',
+  'prefix' => '',
+  'host' => '192.168.2.34',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'driver' => 'mysql',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+// if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+//   include $app_root . '/' . $site_path . '/settings.local.php';
+//  }
  
 $settings['config_sync_directory'] = 'sites/default/files/config_e7sfg_0vMZIQqDDs2haPlIfWAbkjd5mMLmtoTE9Luk-EGo6nHQm_SqNAaMMK856oOvTHWCuCZA/sync';

199279c28e94b641c2eaf7d1232f50b21fe66302
Wisdom <wisdom.houede@smile.ci>
2024-07-25 15:41:54 +0000

    merge ok

--- src/web/modules/custom/yelen_faq/src/Controller/DefaultController.php
+++ src/web/modules/custom/yelen_faq/src/Controller/DefaultController.php
@@ -3,9 +3,15 @@
 
 namespace Drupal\Yelen_faq\Controller;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class DefaultController extends ControllerBase
 {
+  public function index(Request $request){
+    return [
+      '#theme'=>'yelen_faq_page'
+    ];
+  }
 
 }

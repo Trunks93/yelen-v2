11365505573d112f9be2310290c7d89162a51699
Wisdom <wisdom.houede@smile.ci>
2024-08-28 15:27:39 +0000

    ok html email template

--- src/web/modules/custom/yelen_notification/src/Services/SendEmailNotification.php
+++ src/web/modules/custom/yelen_notification/src/Services/SendEmailNotification.php
@@ -16,34 +16,39 @@ class SendEmailNotification
    * @param array|null $templateHtml
    * @return bool
    */
-  public function sendNotification(string $subject, string $to, string $cc = null, array $templateHtml = null):bool
+  public function sendNotification(string $subject, string $to, string $cc = null, array $templateHtml = null): bool
   {
     try {
       $mailManager = \Drupal::service('plugin.manager.mail');
       $langcode = \Drupal::currentUser()->getPreferredLangcode();
       $params = [];
       $params['headers'] = [
-        'Content-Type' => 'text/html; charset=UTF-8;',
-        'Content-Transfer-Encoding' => '8Bit',
+        "MIME-Version" => "1.0",
+        "Content-Type" => "text/html; charset=UTF-8; format=flowed; delsp=yes",
+        "Content-Transfer-Encoding" => "8Bit"
       ];
-      $params['id'] ='mail';
+      $params['id'] = 'mail';
       if ($cc !== null) {
         $params['headers']['Cc'] = $cc;
       }
       if ($templateHtml != null) {
         $params['message'] = \Drupal::service('renderer')->render($templateHtml);
+        //$params['message'] = $templateHtml;
       }
-      $params['subject'] = $subject;
+      $params['formatter'] = "phpmailer_smtp";
 
-      $message =  $mailManager->mail(self::MODULE_NAME, self::MODULE_NAME, $to, $langcode, $params, NULL, false);
+      $message = $mailManager->mail(self::MODULE_NAME, self::MODULE_NAME, $to, $langcode, $params, NULL, false);
+      $message['body'] = $params['message'];
+      $message['subject'] = $subject;
+
 
       $phpMailerSmtp = $mailManager->createInstance('phpmailer_smtp');
 
       $result['result'] = $phpMailerSmtp->mail($message);
 
-      $this->logSendNotification($result,$subject);
-    }catch (\Exception $e){
-      $this->logSendNotification([],$subject,$e);
+      $this->logSendNotification($result, $subject);
+    } catch (\Exception $e) {
+      $this->logSendNotification([], $subject, $e);
       $result = false;
       throw new \Exception($e->getMessage());
     }
@@ -57,14 +62,15 @@ class SendEmailNotification
    * @param $subject
    * @param null $error
    */
-  private function logSendNotification(array $result,$subject, $error = null){
+  private function logSendNotification(array $result, $subject, $error = null)
+  {
     if ($result['result'] == true) {
-      \Drupal::logger('yelen_notification')->info("Notification envoyé pour : ".strtoupper($subject));
-    }else{
-      if($error instanceof \Exception){
+      \Drupal::logger('yelen_notification')->info("Notification envoyé pour : " . strtoupper($subject));
+    } else {
+      if ($error instanceof \Exception) {
         \Drupal::logger('yelen_notification')->critical($error->getMessage());
-      }else{
-        \Drupal::logger('yelen_notification')->error("Erreur notification pour : ".strtoupper($subject));
+      } else {
+        \Drupal::logger('yelen_notification')->error("Erreur notification pour : " . strtoupper($subject));
       }
     }
   }

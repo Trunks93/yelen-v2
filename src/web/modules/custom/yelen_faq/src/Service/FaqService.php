c139a2809b1df8a3d25f1e59fa2be28372ac2adc
Wisdom <wisdom.houede@smile.ci>
2024-09-02 15:43:16 +0000

    little changer

--- src/web/modules/custom/yelen_faq/src/Service/FaqService.php
+++ src/web/modules/custom/yelen_faq/src/Service/FaqService.php
@@ -47,7 +47,7 @@ class FaqService
           'name'=>$parent->label(),
           'description'=>$parent->getDescription(),
           'parent'=>$parent->parent->getValue(),
-          'image'=>
+          //'image'=>
         ]
       }*/
       return $parentUniques;

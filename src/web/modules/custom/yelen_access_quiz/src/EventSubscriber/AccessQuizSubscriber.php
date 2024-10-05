9976740e11340b29b5873a8a9641483229d4293f
Wisdom <wisdom.houede@smile.ci>
2024-10-05 01:05:02 +0000

    add pathauto module and refactor

--- src/web/modules/custom/yelen_access_quiz/src/EventSubscriber/AccessQuizSubscriber.php
+++ src/web/modules/custom/yelen_access_quiz/src/EventSubscriber/AccessQuizSubscriber.php
@@ -3,12 +3,7 @@
 
 namespace Drupal\yelen_access_quiz\EventSubscriber;
 
-
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Site\Settings;
 use Drupal\Core\Routing\RouteSubscriberBase;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Routing\RouteCollection;
 
 class AccessQuizSubscriber extends RouteSubscriberBase //implements EventSubscriberInterface
@@ -17,11 +12,17 @@ class AccessQuizSubscriber extends RouteSubscriberBase //implements EventSubscri
   {
       $quiz_route = $collection->get('entity.quiz.take');
       $question_route = $collection->get('quiz.question.take');
+      $quiz_canonical = $collection->get('entity.quiz.canonical');
+
       $quiz_route->setRequirements([
         '_custom_access' => 'Drupal\yelen_access_quiz\Controller\AccessQuizController::has_access'
       ]);
 
       $question_route->setRequirements([
+        '_custom_access' => 'Drupal\yelen_access_quiz\Controller\AccessQuizController::has_access'
+      ]);
+
+      $quiz_canonical->setRequirements([
         '_custom_access' => 'Drupal\yelen_access_quiz\Controller\AccessQuizController::has_access'
       ]);
     }

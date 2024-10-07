8ccb2a418dba1a022869737663b7d97de82a915b
Saint Cyr <cyr.kpe@smile.ci>
2024-10-07 13:17:40 +0000

    Config Chat App

--- src/web/modules/custom/orange_yelen_chat/chat-app/vite.config.ts
+++ src/web/modules/custom/orange_yelen_chat/chat-app/vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import {resolve} from 'node:path';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,9 +8,29 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    lib: {
+      name: 'Chat App',
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['umd'],
+      fileName(format, entryName) {
+        return `chat-app.${format}.js`
+      },
+    },
+    rollupOptions: {
+      input: resolve(__dirname, 'src/main.ts'),
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/': 'http://yelen-v2.local/chat-app',
+    },
   }
 })

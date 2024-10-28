6f14da4f71c18cc9b933eaba505d1fe8ddcb4295
Wisdom <wisdom.houede@smile.ci>
2024-10-28 16:03:20 +0000

    add video module

--- src/web/modules/custom/yelen_notification/src/Constante/Constante.php
+++ src/web/modules/custom/yelen_notification/src/Constante/Constante.php
@@ -7,7 +7,7 @@ namespace Drupal\yelen_notification\Constante;
 class Constante
 {
 
-const UNAUTHORIZED_NODES = ['article','page','forum','sujet'];
+const UNAUTHORIZED_NODES = ['article','page','forum','sujet','a_la_une'];
 
 const PUBLISHED = 'published';
 const DRAFT = 'draft';

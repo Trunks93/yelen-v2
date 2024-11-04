bf08c2f33c88b0e259785050f05da78750c5ba4d
Wisdom <wisdom.houede@smile.ci>
2024-11-04 13:45:53 +0000

    add popup

--- src/web/modules/custom/yelen_notification/src/Constante/Constante.php
+++ src/web/modules/custom/yelen_notification/src/Constante/Constante.php
@@ -7,7 +7,7 @@ namespace Drupal\yelen_notification\Constante;
 class Constante
 {
 
-const UNAUTHORIZED_NODES = ['article','page','forum','sujet','a_la_une'];
+const UNAUTHORIZED_NODES = ['article','page','forum','sujet','a_la_une','popup'];
 
 const PUBLISHED = 'published';
 const DRAFT = 'draft';

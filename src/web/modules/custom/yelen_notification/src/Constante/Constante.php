1166c630f99a5a428cbccada82053a85951c35a3
Wisdom <wisdom.houede@smile.ci>
2024-11-06 12:45:05 +0000

    ok

--- src/web/modules/custom/yelen_notification/src/Constante/Constante.php
+++ src/web/modules/custom/yelen_notification/src/Constante/Constante.php
@@ -6,8 +6,6 @@ namespace Drupal\yelen_notification\Constante;
 
 class Constante
 {
-
-
 const UNAUTHORIZED_NODES = ['article','page','forum','sujet','a_la_une','popup'];
 
 const PUBLISHED = 'published';

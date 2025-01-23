4c6b03b03c56c1474640b1cd6fee2845e36db626
Wisdom <wisdom.houede@smile.ci>
2025-01-23 14:52:04 +0000

    ok

--- src/web/themes/custom/yelen_theme/assets/js/app.js
+++ src/web/themes/custom/yelen_theme/assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
   $('.slicker-images').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -6,6 +6,17 @@ $(document).ready(function(){
     autoplaySpeed: 2000,
   });
 });
+*/
 
+var swiper = new Swiper(".slicker-images", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 

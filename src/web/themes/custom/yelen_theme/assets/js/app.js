1d385cd4c6c4bea24fbec47df76502f235184237
Wisdom <wisdom.houede@smile.ci>
2025-01-24 00:35:19 +0000

    end carousel

--- src/web/themes/custom/yelen_theme/assets/js/app.js
+++ src/web/themes/custom/yelen_theme/assets/js/app.js
@@ -8,10 +8,13 @@
 });
 */
 
-var swiper = new Swiper(".slicker-images", {
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
-    type: "progressbar",
+    clickable: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",

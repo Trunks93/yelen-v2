7cecbbca0b7605ba40d0451dfd49fc0bbf8253d5
Pagab <sanogoabou32@gmail.com>
2025-01-29 12:28:27 +0000

    Update ui design page accueil

--- src/web/modules/custom/yelen_faq/assets/js/app.js
+++ src/web/modules/custom/yelen_faq/assets/js/app.js
@@ -5,7 +5,14 @@ $(document).ready(function(){
     autoplay: true,
     autoplaySpeed: 2000,
   });
-});
 
+  $('#faq-block').find(sub-category-link).on('check', function(e){
+    console.log('Test dsj')
+    $('.sub-category-link').find(orange-color-link).on('click', function(e){
+        console.log('Test')
+    })
+  })
+
+});
 
 

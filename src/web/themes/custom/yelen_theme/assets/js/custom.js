f2d37c2a89b9c8e2b2f9dd1ecf2f6a228ba0bdaf
Saint Cyr <cyr.kpe@smile.ci>
2025-06-23 13:31:39 +0000

    Reload access denied page after 50ms and add spinner when reloading

--- src/web/themes/custom/yelen_theme/assets/js/custom.js
+++ src/web/themes/custom/yelen_theme/assets/js/custom.js
@@ -1,43 +1,20 @@
 
-  console.log('---CUSTOM JS----')
-  console.log('---CUSTOM JS - Document Title----', document.title)
-  let checkLoginExecuted = false;
-  const isPageReloaded = sessionStorage.getItem('isAccessDeniedPageReloaded')
-  console.log('---isPageReloaded----', isPageReloaded)
-  const accessDeniedPageClass = document.body.querySelector('.body.access-denied-page')
-  const isAccessDeniedPage = accessDeniedPageClass && (document.title.includes('Access denied') || document.title.includes('Accès refusé'))
-  if(isAccessDeniedPage && !isPageReloaded){
-    setTimeout(() => {
-      console.log('----Page Reloaded after 500ms-----')
-      sessionStorage.setItem('isAccessDeniedPageReloaded', 'true')
-      location.reload();
-    }, 500);
-    setTimeout(() => {
-      sessionStorage.removeItem('isAccessDeniedPageReloaded')
-    }, 5000);
-  }
-
-  /*
-  (function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.checkLogin = {
-    attach: function (context, settings) {
-      const accessDeniedPageClass = document.body.querySelector('.body.access-denied-page')
-      const isAccessDeniedPage = accessDeniedPageClass && (document.title.includes('Access denied') || document.title.includes('Accès refusé'))
-      console.log('---isAccessDeniedPage---', isAccessDeniedPage)
-      console.log('---accessDeniedPageClass---', accessDeniedPageClass)
-      console.log('---drupalSettings.user.isLoggedIn---', drupalSettings.user.isLoggedIn)
-      if(!isAccessDeniedPage) return;
-      if(checkLoginExecuted || isPageReloaded || !drupalSettings.user.isLoggedIn) return;
-      checkLoginExecuted = true
-      setTimeout(() => {
-        location.reload();
-        sessionStorage.setItem('isAccessDeniedPageReloaded', true)
-      }, 500);
-      setTimeout(() => {
-        sessionStorage.removeItem('isAccessDeniedPageReloaded')
-      }, 600);
-    }
-  };
-})(jQuery, Drupal, drupalSettings);
-
-   */
+console.log('---CUSTOM JS----')
+console.log('---CUSTOM JS - Document Title----', document.title)
+let checkLoginExecuted = false;
+const isPageReloaded = sessionStorage.getItem('isAccessDeniedPageReloaded')
+console.log('---isPageReloaded----', isPageReloaded)
+const accessDeniedPageClass = document.body.querySelector('.body.access-denied-page')
+const isAccessDeniedPage = accessDeniedPageClass && (document.title.includes('Access denied') || document.title.includes('Accès refusé'))
+if(isAccessDeniedPage && !isPageReloaded){
+  document.title = "Chargement..."
+  document.body.querySelector('.unauthorized').innerHTML = '<div class="spinner-border" role="status"><span class="visually-hidden">Chargement...</span></div>'
+  setTimeout(() => {
+    console.log('----Page Reloaded after 50ms-----')
+    sessionStorage.setItem('isAccessDeniedPageReloaded', 'true')
+    location.reload();
+  }, 50);
+}
+setTimeout(() => {
+  sessionStorage.removeItem('isAccessDeniedPageReloaded')
+}, 1000);
